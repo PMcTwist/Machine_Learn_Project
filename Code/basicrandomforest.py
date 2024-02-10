6f913dfdef4c325d0bc50784510d66493ca87b1c
P@ <patmaynard452@gmail.com>
2024-02-09 21:39:09 -0500

    -Imported the real data to work with -Implemented a conversion loop to change the date strings into timestamp integers -Fixed the Encoder to work with the dataset -Fixed the HPI data to be a 2D array -Got a prediction to output -Got the first plot we are working with
    P@
    
    Signed-off-by: P@ <patmaynard452@gmail.com>

--- Code/basicrandomforest.py
+++ Code/basicrandomforest.py
@@ -11,8 +11,9 @@ Import Libraries
 
 # basic imports
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-
+from datetime import datetime
 
 # specific imports
 from sklearn.model_selection import train_test_split
@@ -23,28 +24,39 @@ from sklearn.preprocessing import LabelEncoder
 """Import Dataset"""
 
 # Assign data to variable
-dataset = pd.read_csv('placeholder.csv')
+dataset = pd.read_csv('combinedData.csv')
 
 # Take a small look at the dataframe produced
 print(dataset.head())
 
 """Preprocess Data (normalize it!)"""
 
-# select columns that need to be processed
+# Convert the date strings into datetime objects
+converted_dates = []
+
+for date in dataset.columns:
+  datetime_object = datetime.strptime(date, '%B %Y')
+  converted_dates.append(datetime_object.timestamp())
+
+dates_array = np.array(converted_dates)
 
 """Encode Catagorical data"""
 
 # Initialize the encoder to work with
 label_encoder = LabelEncoder()
 
+print(dataset.iloc[0])
+
 # Fit the data needed to the encoder for later use
-dataset['column name'] = label_encoder.fit_transform(dataset['column name'])
+label_encoder.fit(dataset.iloc[0])
+
+dataset.iloc[1] = label_encoder.transform(dataset.iloc[0])
 
 """Split data into X and y sets"""
 
 # define X and y variables
-X = dataset.iloc[:, 1].values
-y = dataset.iloc[:, 2].values
+X = dates_array
+y = dataset.iloc[0].values
 
 # varify the split data
 print(f"X dataset example: {X[0]}")
@@ -55,10 +67,13 @@ print(f"y dataset example: {y[0]}")
 # Split into 4 specific variables
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+X_train = X_train.reshape(-1, 1)
+X_test = X_test.reshape(-1, 1)
+
 """Instatiate the model"""
 
 # plant the seeds to grow a forest
-forest_model = RandomForestRegressor(n_estimators=50, random_state=42)
+forest_model = RandomForestRegressor(n_estimators=50, random_state=0)
 
 """Train the model"""
 
@@ -69,6 +84,8 @@ forest_model.fit(X_train, y_train)
 
 # pass in X_test to predict a y value
 y_predict = forest_model.predict(X_test)
+
+print(y_predict)
 
 """Check the model"""
 
